d25369254d014ee3a59413e815fb7a064b532d8d
sabertazimi <sabertazimi@gmail.com>
2026-02-28 22:08:29 +0800

    fix(nvim-keymaps): add rebuild dictionary keymaps

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local wk = require("which-key")
+
+local spell_lang = "en"
+local spell_file = vim.fn.stdpath("config") .. "/spell/" .. spell_lang .. ".utf-8.add"
+
+wk.add({
+  {
+    "<leader>um",
+    function()
+      vim.cmd("mkspell! " .. spell_file)
+      vim.notify("Spell dictionary rebuilt: " .. spell_file, vim.log.levels.INFO)
+    end,
+    desc = "Rebuild Spell Dictionary",
+    icon = "󰓫 ",
+  },
+})
